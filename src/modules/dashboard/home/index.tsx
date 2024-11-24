6b581b18ecdd0a5d7cdb9c4aca5074322c43a2dc
Adetayo1999 <omotomiwaadetayo1102@gmail.com>
2024-11-24 18:43:56 +0100

    chore: updated the codebase

--- src/modules/dashboard/home/index.tsx
+++ src/modules/dashboard/home/index.tsx
@@ -1,11 +1,35 @@
+import { useQuery } from "react-query";
 import { DashboardCard } from "../../../components/card";
 import {
   SwatchIcon,
   DocumentIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/solid";
+import { EventType } from "react-hook-form";
+import services from "../../../services";
+import { PostType, QuestionnaireType } from "../../../lib/types";
 
 export default function Home() {
+  const { data } = useQuery<EventType[]>(["events"], services.getEvents, {
+    refetchOnWindowFocus: false,
+  });
+
+  const { data: postData } = useQuery<PostType[]>(
+    ["posts"],
+    services.getPosts,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
+  const { data: questionnaireData } = useQuery<QuestionnaireType[]>(
+    ["questionnaire"],
+    services.getQuestionnaires,
+    {
+      refetchOnWindowFocus: false,
+    }
+  );
+
   return (
     <div className="p-12 pt-16">
       <div className="mb-10">
@@ -14,11 +38,19 @@ export default function Home() {
         </h1>
       </div>
       <div className="grid grid-cols-3 gap-5 gap-x-14">
-        <DashboardCard metric={400} title="Total Blogs" Icon={SwatchIcon} />
-        <DashboardCard metric={10} title="Total Events" Icon={DocumentIcon} />
         <DashboardCard
-          metric={2}
-          title="Total Blog Categories"
+          metric={postData?.length || 0}
+          title="Total Blogs"
+          Icon={SwatchIcon}
+        />
+        <DashboardCard
+          metric={data?.length || 0}
+          title="Total Events"
+          Icon={DocumentIcon}
+        />
+        <DashboardCard
+          metric={questionnaireData?.length || 0}
+          title="Total Questionnaires"
           Icon={UserGroupIcon}
         />
       </div>
